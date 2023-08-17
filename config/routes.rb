d8563f473c9358ff5b085826decd46e19e19e303
Oleksandra Tovshchyk <alexestov@gmail.com>
2023-08-17 23:08:58 +0200

    challenge without css done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
